027d5a9eecad700507a366a576474ee6250ff28d
laranlopez <98132014+laranlopez@users.noreply.github.com>
2022-02-11 20:06:49 -0300

    homeW 6

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,12 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+
+  var nuevoArray = []
+  for (var clave in objeto){
+    nuevoArray.push([clave,objeto[clave]})
+  }
+  return nuevoArray
 }
 
 
@@ -18,6 +24,8 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+
+  
 }
 
 
